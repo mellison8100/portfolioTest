d3201c6a2d6913e4320065f83b041310daff2d0d
Max Ellison <maximum8100@gmail.com>
2017-04-04 12:28:18 -0500

    Add files via upload

---
+++ epilepsydotgov/epilepsydotgov.js
@@ -0,0 +1,49 @@
+function setup() {
+   createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(random(255),random(255),random(255));
+  
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  fill(random(255),random(255),random(255));
+  ellipse(random(25,windowWidth-25),random(25,975),50,50);
+  
+}
